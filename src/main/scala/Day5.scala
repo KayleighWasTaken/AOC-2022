3dad5ccdf73ce16c25503a162d7b7997923f2f28
Kayleigh <41625824+KayleighWasTaken@users.noreply.github.com>
2022-12-05 22:07:54 +0000

    refactor day 5 to parse properly

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -4,8 +4,8 @@ import scala.io.Source
 
 @main @nowarn def main = {
   val input = Using.resource(Source.fromFile("input-day5.txt"))(_.getLines.toList)
-  val stacks = parseStacks(input.take(8))
-  val moves = input.drop(10).map(_.split(' ').collect {case x if x.forall(_.isDigit) => x.toInt})
+  val stacks = parseStacks(input)
+  val moves = parseMoves(input)
   val partOne = moves.foldLeft(stacks) {
     case (stacks, Array(amount, from, to)) =>
       val (movedCrates, newFromStack) = stacks(from - 1).splitAt(amount)
@@ -23,5 +23,12 @@ import scala.io.Source
 }
 
 def parseStacks(xs: List[String]): List[List[Char]] = {
-  (1 to 33 by 4).map(i => xs.collect {case s if s.isDefinedAt(i) && s(i) != ' ' => s(i)}).toList
+  val stackCount = xs.collectFirst{case ln if ln.last.isDigit => ln.split(' ').last.toInt - 1}.getOrElse(0)
+  val stackLines = xs.takeWhile(_.last == ']')
+  (1 to (stackCount * 4 + 1) by 4).map(i => stackLines.collect {case s if s.isDefinedAt(i) && s(i) != ' ' => s(i)}).toList
+}
+
+def parseMoves(xs: List[String]): List[Array[Int]] = {
+  val moveStrings = xs.dropWhile(!_.startsWith("move"))
+  moveStrings.map(_.split(' ').collect {case x if x.forall(_.isDigit) => x.toInt}).filter(_.length == 3)
 }
